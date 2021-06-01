f7e31b4839ba5d70fb3f8d2859253e15f198a2da
Rysiek <rysiexo2@gmail.com>
2021-06-01 18:34:33 +0200

    Initial commit.

--- src/main/java/com/mas/project/ProjectApplication.java
+++ src/main/java/com/mas/project/ProjectApplication.java
@@ -1,7 +1,10 @@
 package com.mas.project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ProjectApplication {
